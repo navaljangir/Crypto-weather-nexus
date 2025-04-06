9c2302510de6f52e47c03b42de5a9cb980f12bd7
NAVAL JANGIR <jangirnaval392003@gmail.com>
2025-04-06 18:11:14 +0530

    removed api

--- app/api/coingekko.ts/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export default async function handler() {
-    const response = await fetch("https://api.coingecko.com/api/v3/coins/solana?localization=false&tickers=false");
-    const data = await response.json();
-    return NextResponse.json(data , {status : 200});
-}
